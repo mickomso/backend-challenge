94167a3fd7742d58395363e9ae3afc211d58040b
Mike <miguelcompanysoler@gmail.com>
2020-11-16 20:33:46 +0100

    Initial project commit

---
+++ src/main/java/com/creditas/challenge/Challenge.java
@@ -0,0 +1,13 @@
+package com.creditas.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge.class, args);
+	}
+
+}
---
+++ src/test/java/com/creditas/challenge/ChallengeTests.java
@@ -0,0 +1,13 @@
+package com.creditas.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
